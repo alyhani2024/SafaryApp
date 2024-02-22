89c11f498b4850e0524eea940a53396077908de7
alyhani2024 <ahani3012@gmail.com>
2024-02-22 22:55:07 +0200

    Update menuData.tsx

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -4,108 +4,32 @@ const menuData: Menu[] = [
     {
       id: 1,
       title: "Home",
-      path: "/",
+      path: "/Tourist/",
       newTab: false,
     },
     {
       id: 2,
       title: "About",
-      path: "/about",
+      path: "/Tourist/about",
       newTab: false,
     },
       {
       id: 3,
       title: "Support",
-      path: "/contact",
+      path: "/Tourist/contact",
       newTab: false,
     },
     {
       id: 33,
       title: "Blog",
-      path: "/blog",
+      path: "/Tourist/blog",
       newTab: false,
     },
-    
-  
     {
       id: 6,
       title: "Tour",
-      path: "/Tour",
+      path: "/Tourist/Tour",
       newTab: false,
-    },
-    // {
-    //   id: 5,
-    //   title: "TourDay",
-    //   path: "/TourDay",
-    //   newTab: false,
-    // },
-    {
-      id: 4,
-      title: "Pages",
-      newTab: false,
-      submenu: [
-        {
-          id: 41,
-          title: "About Page",
-          path: "/about",
-          newTab: false,
-        },
-        {
-          id: 42,
-          title: "Contact Page",
-          path: "/contact",
-          newTab: false,
-        },
-        {
-          id: 43,
-          title: "Blog Grid Page",
-          path: "/blog",
-          newTab: false,
-        },
-        {
-          id: 44,
-          title: "Blog Sidebar Page",
-          path: "/blog-sidebar",
-          newTab: false,
-        },
-        {
-          id: 45,
-          title: "Blog Details Page",
-          path: "/blog-details",
-          newTab: false,
-        },
-        {
-          id: 46,
-          title: "Sign In Page",
-          path: "/signin",
-          newTab: false,
-        },
-        {
-          id: 47,
-          title: "Sign Up Page",
-          path: "/signup",
-          newTab: false,
-        },
-        {
-          id: 48,
-          title: "Error Page",
-          path: "/error",
-          newTab: false,
-        },
-        // Add TourHour and TourDay routes here
-        {
-          id: 49,
-          title: "Tour",
-          path: "/Tour",
-          newTab: false,
-        },
-        // {
-        //   id: 50,
-        //   title: "TourDay",
-        //   path: "/TourDay",
-        //   newTab: false,
-        // },
-      ],
     },
   ];
   
