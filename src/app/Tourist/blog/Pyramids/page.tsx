33627011da94984b0d9b691f72c5ec5714d9b802
alyhani2024 <ahani3012@gmail.com>
2024-02-24 16:56:41 +0200

    Add Payment page

--- src/app/Tourist/blog/Pyramids/page.tsx
+++ src/app/Tourist/blog/Pyramids/page.tsx
@@ -94,7 +94,7 @@ const BlogDetailsPage = () => {
                   </div>
                   <div className="mb-5">
                     <a
-                      href="#0"
+                      href="/Tourist/payment"
                       className="inline-flex items-center justify-center rounded-full bg-orange-500 px-4 py-2 text-sm font-semibold text-white"
                     >
                       Join In
