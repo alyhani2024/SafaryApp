4ec5fa19c878aa259e51896e4e38a2b908374e28
alyhani2024 <159263533+alyhani2024@users.noreply.github.com>
2024-05-08 01:52:06 +0200

    Create table

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,6 +1,6 @@
 import Breadcrumb from '@/components/Common/Breadcrumb'
 import ScrollUp from '@/components/Common/ScrollUp'
-import Hero from '@/components/Hero'
+import UserTable from '@/components/Table/TableUser'
 import ToDoList from '@/components/To-Do/TodoList'
 import React from 'react'
 
@@ -13,6 +13,11 @@ const Adminpage = () => {
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
 
+      <UserTable/>
+      <br/>
+      <hr/>
+      <br/>
+      <hr/>
       <ToDoList/>
 
 
