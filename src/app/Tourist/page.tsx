c94ab1ae7e9b228d6cb207326d78cbb282381dbb
Moustafa Batt <zaftt007@gmail.com>
2024-05-13 23:26:01 +0200

    add a carousel tour guides per hour

--- src/app/Tourist/page.tsx
+++ src/app/Tourist/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Blog from "@/components/Blog";
+import CarouselPerHour from "@/components/CarouselPerHour/CarouselPerHour";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
@@ -8,6 +9,66 @@ import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
 
 
+
+const tourGuidesPerHour = [
+  {
+    name: 'John Doe',
+    description: 'Experienced tour guide with a passion for history.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'Jane Smith',
+    description: 'Friendly and knowledgeable guide specializing in outdoor adventures.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'John Doe',
+    description: 'Experienced tour guide with a passion for history.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'Jane Smith',
+    description: 'Friendly and knowledgeable guide specializing in outdoor adventures.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'John Doe',
+    description: 'Experienced tour guide with a passion for history.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'Jane Smith',
+    description: 'Friendly and knowledgeable guide specializing in outdoor adventures.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'John Doe',
+    description: 'Experienced tour guide with a passion for history.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  {
+    name: 'Jane Smith',
+    description: 'Friendly and knowledgeable guide specializing in outdoor adventures.',
+    photoUrl: '/images/Home/pexels-omar-elsharawy-5609738.jpg',
+    rate: 4,
+  pricePerHour: 100
+  },
+  // Add more tour guides as needed
+];
 export default function HomeTourist() {
   return (
     <>
@@ -15,9 +76,11 @@ export default function HomeTourist() {
       <Hero />
       <Features />
       <Testimonials />
+      <CarouselPerHour tourGuides={tourGuidesPerHour} />
       <Pricing />
       <Blog />
       <Contact />
+
     </>
   );
 }
