bbbf024aea15a6f9e9302e776161097ae3dd66e2
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-05-18 14:22:55 +0200

    Update Main page

--- src/components/BookBlogs/BookBlogs.tsx
+++ src/components/BookBlogs/BookBlogs.tsx
@@ -1,39 +1,38 @@
-import { ArrowDown } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
-// import  './index.scss';
+
 const BookBlogs = () => {
   return (
     <>
       <section
         id="home"
-        className="relative z-10 overflow-hidden   pb-16 pt-[120px]   md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="relative z-10 overflow-hidden pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
-        <Image src={"/images/hero/pexels-valentinantonucci-841286.jpg"} width={2000} height={2000} alt="img" className="imgHome  absolute  top-0 "/>
-        {/* <video autoPlay muted loop className="video-background imgHome  absolute  top-0">
-          <source
-            src="https://video.wixstatic.com/video/11062b_9de2dbff3dda403b944bb98c41cb5764/1080p/mp4/file.mp4"
-            type="video/mp4"
-          /> </video> */}
-        <div className="container ">
+        <Image
+          src="/images/Home/blogs.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="img"
+          className="absolute inset-0 w-full h-full"
+        />
+        <div className="container relative z-20">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-dark sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Book your Blog 
+              <div className="mx-auto max-w-[800px] text-center text-white">
+                <h1 className="mb-5 text-3xl font-bold leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Book your Blog
                 </h1>
-                <p className="mb-12 text-base !leading-relaxed text-dark sm:text-lg md:text-xl">
-                The Safary application aims to revolutionize the tourism industry by connecting tourists with local tour guides in Egypt. 
-                This innovative platform will provide tourists with easy access to experienced tour guides, enabling them to explore Egypt rich cultural heritage and historical landmarks.
+                <p className="mb-12 text-base leading-relaxed sm:text-lg md:text-xl">
+                  The Safary application aims to revolutionize the tourism industry by connecting tourists with local tour guides in Egypt. 
+                  This innovative platform will provide tourists with easy access to experienced tour guides, enabling them to explore Egypts rich cultural heritage and historical landmarks.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="/Tourist/BookBlog"
-                    className="transition duration-1500  rounded-sm bg-orange-500 px-8 py-4 text-base font-semibold text-white  ease-in-out hover:bg-orange-500/80 "
+                    className="transition duration-1500 rounded-sm bg-orange-500 px-8 py-4 text-base font-semibold text-white ease-in-out hover:bg-orange-500/80"
                   >
                     <span>Book Now</span>
                   </Link>
-                 
                 </div>
               </div>
             </div>
