088896a0e181f42f12a9d5c4310abef0bf492ee9
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-07-05 13:35:20 +0200

    updatAdmin

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,26 +1,21 @@
-import Breadcrumb from '@/components/Common/Breadcrumb'
-import ScrollUp from '@/components/Common/ScrollUp'
-import ProductsPage from '@/components/Table/NewTable'
-import UserApiTable from '@/components/Table/UserApi'
-import UserTable from '@/components/Table/AdminTable'
-import TodoComponent from '@/components/todo/TodoBlog'
-import React from 'react'
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import ScrollUp from "@/components/Common/ScrollUp";
+import UserTable from "@/components/Table/AdminTable";
+import React from "react";
 
 const Adminpage = () => {
   return (
-	<>
-	<ScrollUp />
-	<Breadcrumb
+    <>
+      <ScrollUp />
+      <Breadcrumb
         pageName=" dashboard"
         description=" Table Of All Users For All Data make Cruds ( Add , Edit and Update ) ."
       />
 
-      <UserTable/>
-      <br/>
+      <UserTable />
+      <br />
+    </>
+  );
+};
 
-
-	</>
-  )
-}
-
-export default Adminpage
+export default Adminpage;
