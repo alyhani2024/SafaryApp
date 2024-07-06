069c7541a3d162ed3870584e7da7a4e7df14e3fa
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-07-06 14:58:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "safaryapi.runasp.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+{/* 
+  
+const nextConfig = {
   images: {
     domains: ["localhost"],
     remotePatterns: [
@@ -19,4 +37,4 @@ module.exports = {
     domains: ['safaryapi.runasp.net'], // Add your API hostname here
   },
 };
-
+  */ }
